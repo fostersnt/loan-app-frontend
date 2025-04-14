f56e48695065e669347e17299a81ab5e205fd9d4
Foster Amponsah <foster.asante@gwosevo.com>
2025-04-14 14:16:36 +0000

    Update

--- components/SwiperThree.jsx
+++ components/SwiperThree.jsx
@@ -6,22 +6,22 @@ import appColors from "@/utils/appColors";
 export default function SliderOne() {
   const screenWidth = Dimensions.get("screen").width;
   const screenHeight = Dimensions.get("screen").height;
-  console.log("WIDTH: ", screenWidth);
-  console.log("HEIGHT: ", screenHeight);
+  
+  const font_size = screenWidth / 10;
 
   return (
     <View style={styles.main_container}>
       <View style={styles.container_1}>
-        <Text style={styles.textStyle}>Flexible repayment plan</Text>
-      </View>
-      <View style={styles.container_2}>
-        <Text style={styles.textStyle}>
-        With LoanGH, enjoy flexible repayment options tailored to bring you peace of mind.
-        </Text>
+        <Text style={[styles.textStyle_1, {fontSize: font_size}]}>Flexible repayment plan</Text>
       </View>
       {/* <View> */}
       <Image style={styles.img} source={img} />
       {/* </View> */}
+      <View style={styles.container_2}>
+        <Text style={styles.textStyle_2}>
+        With LoanGH, enjoy flexible repayment options tailored to bring you peace of mind.
+        </Text>
+      </View>
       <View style={styles.container_3}>
         <TouchableOpacity>
           <Text style={styles.btnText}>Get Started</Text>
@@ -61,9 +61,16 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "50%",
   },
-  textStyle: {
+  textStyle_1: {
     fontSize: 20,
     // fontWeight: "bold",
-    color: "appColors.orange_two"
+    textAlign: "center",
+    color: appColors.dark_one
+  },
+  textStyle_2: {
+    fontSize: 20,
+    // fontWeight: "bold",
+    textAlign: "center",
+    color: appColors.dark_one
   },
 });
