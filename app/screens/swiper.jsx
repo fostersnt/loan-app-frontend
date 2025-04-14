f44bfe99124251a2bd43083183b9b54ab7791fd6
Foster Amponsah <foster.asante@gwosevo.com>
2025-04-14 13:17:08 +0000

    Update

--- app/screens/swiper.jsx
+++ app/screens/swiper.jsx
@@ -50,7 +50,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   dotStyle: {
-    backgroundColor: appColors.white,
+    backgroundColor: appColors.dark_one,
     width: 10,
     height: 10,
     borderRadius: 20,
