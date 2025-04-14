c902af7c230a499598185cd4c4a23f2b5fadd563
Foster Amponsah <foster.asante@gwosevo.com>
2025-04-14 11:03:14 +0000

    Update

--- app/index.jsx
+++ app/index.jsx
@@ -1,31 +1,46 @@
 import appColors from "@/utils/appColors";
-import { StatusBar, StyleSheet, View } from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Image, StatusBar, StyleSheet, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import {Video} from "expo-av";
+import bankNote from "../assets/images/Banknote.png";
+import { useEffect } from "react";
+import { useNavigation } from "expo-router";
+// const moneyMotivation = require ("../assets/images/Money_motivation.mp4");
 
 export default function Index() {
+
+  const navigate = useNavigation();
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      console.log("HELLO WORLD");
+      navigate.navigate('home');
+    }, 3000)
+  }, [])
+
   return (
-    <View
-      style={styles.mainContainer}
-    >
-      <StatusBar hidden/>
-      <Icon name="money" size={150} color={appColors.white} />
+    <View style={styles.mainContainer}>
+      <StatusBar hidden />
+      {/* <Icon name="money" size={150} color={appColors.white} /> */}
+      <Image source={bankNote} style={styles.splashImageStyle}/>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: appColors.violet_one
+    backgroundColor: appColors.white,
   },
-  splashImageStyle:{
+  splashImageStyle: {
     // color: appColors.white,
-    width: 300,
-    height: 300,
+    width: "100%",
+    height: "50%",
   },
-  textStyle:{
+  textStyle: {
     color: appColors.white,
-  }
-})
+  },
+});
--- utils/appColors.js
+++ utils/appColors.js
@@ -2,5 +2,8 @@ export default appColors = {
     violet_one: "#352077",
     violet_two: "#5145c1",
     white: "#ffffff",
-    orange: "#ff8b28"
-}
+    orange_one: "#ff8b28",
+    color_two: "#FF725E",
+    dark_one: "#455a64",
+    dark_two: "#263238",
+} 
